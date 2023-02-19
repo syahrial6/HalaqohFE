aeb2348acc980c933db227912708d3b374ddadf5
syahrial6 <62192090+syahrial6@users.noreply.github.com>
2023-02-19 19:24:24 +0700

    Update2

--- src/features/AuthSlices.js
+++ src/features/AuthSlices.js
@@ -16,7 +16,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser",async(user,thunkAPI)=
             password : user.password
         },{
             headers: {
-                'content-type':'Autorization'
+                'content-type':'Authorization'
               },
         });
         return response.data
