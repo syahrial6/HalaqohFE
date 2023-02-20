bf6500b6387490aa703ce864c2c9dbbaf6eb9be4
syahrial6 <62192090+syahrial6@users.noreply.github.com>
2023-02-20 10:26:39 +0700

    update77

--- src/features/AuthSlices.js
+++ src/features/AuthSlices.js
@@ -29,18 +29,7 @@ export const LoginUser = createAsyncThunk("user/LoginUser",async(user,thunkAPI)=
     }
 });
 
-export const getMe = createAsyncThunk("user/getme",async(_,thunkAPI)=>{
-    try {
-        const response = await axios.get("https://halaqoh2.my.id/me");
-        return response.data
-    } catch (error) {
-        if (error.response){
-            const message = error.response.data.msg;
-            return thunkAPI.rejectWithValue(message);
-        }
-        
-    }
-});
+
 
 export const Logout = createAsyncThunk("user/Logout",async()=>{
     await axios.delete("https://halaqoh2.my.id/logout");
@@ -70,19 +59,7 @@ export const AuthSlice = createSlice({
         })
 
         // get me
-        builder.addCase(getMe.pending, (state) =>{
-            state.isLoading = true;
-        });
-        builder.addCase(getMe.fulfilled, (state,action)=>{
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.user = action.payload;
-        })
-        builder.addCase(getMe.rejected, (state,action)=>{
-            state.isLoading = false;
-            state.isError = true;
-            state.message = action.payload;
-        })
+      
 }})
 export const {reset} = AuthSlice.actions
 
